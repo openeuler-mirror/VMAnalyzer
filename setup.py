33e7504860fa8add8dc0b2d8af845a8835b4d706
dinglimin_yewu <dinglimin_yewu@cmss.chinamobile.com>
2023-10-27 15:57:17 +0800

    Add collect VM statistics and save into redis storage
    Signed-off-by: dinglimin_yewu <dinglimin_yewu@cmss.chinamobile.com>

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='vm-analyzer',
     packages=find_packages(),
     version='0.1.0',
+    install_requires=[
+        'redis'
+    ],
     entry_points={
         'console_scripts': [
             'vm-analyzer-agent=agent.main:main'
---
+++ utils/timer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# _*_coding: utf-8 _*_
+#######################################################################################
+# Copyright (c) 2023. China Mobile (SuZhou) Software Technology Co.,Ltd.
+# VMAnalyzer is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+#######################################################################################
+import threading
+import time
+
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.next_call = time.time()
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self.next_call += self.interval
+            self._timer = threading.Timer(self.next_call - time.time(), self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
