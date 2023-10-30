2a7da72c7c5e868db28660f1a8cd6c8eb218c502
dinglimin_yewu <dinglimin_yewu@cmss.chinamobile.com>
2023-10-30 14:18:30 +0800

    Add event test
    Signed-off-by: dinglimin_yewu <dinglimin_yewu@cmss.chinamobile.com>

---
+++ tests/unit/agent/test_event.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# _*_coding: utf-8 _*_
+#######################################################################################
+# Copyright (c) 2023. China Mobile (SuZhou) Software Technology Co.,Ltd.
+# VMAnalyzer is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+#######################################################################################
+import unittest
+import mock
+from agent import event
+import libvirt
+
+
+class TestVMEventLoopNative(unittest.TestCase):
+    def test_start(self):
+        with mock.patch.object(
+                libvirt, 'virEventRegisterDefaultImpl'
+        ) as mock_register:
+            ev = event.VMEventLoopNative("qemu:///system")
+            ev.start()
+            self.assertTrue(mock_register.called)
+
+if __name__ == "__main__":
+        unittest.main()
