2b6ee2bfb45ac8661643440dbd8e654eca7944ae
houhaole_yewu <houhaole_yewu@cmss.chinamobile.com>
2025-10-31 10:11:57 +0800

    fix: ensure last valid CPU utilization is saved in setVMAnalyzers
    Signed-off-by: houhaole_yewu <houhaole_yewu@cmss.chinamobile.com>

--- agent/analyze.py
+++ agent/analyze.py
@@ -31,33 +31,35 @@ class VMStatsAnalyze(object):
 
         analyzers_list = []
         logging.debug('Length of VM stats: %d', len(vmStatsInfo))
+        
+        last_cpu_util = 0.0
+
         for i in range(len(vmStatsInfo) - 1):
             assert vmStatsInfo[i]['uuid'] == vmStatsInfo[i+1]['uuid']
-            # User can adjust the number of vcpus???
             vcpu_count = vmStatsInfo[i]['vcpus']
             logging.debug('VM %s: previous cputime: %ld, latter cputime: %ld, '
                           'previous timestamp: %d, latter timestamp: %d',
                           vm_info['name'], vmStatsInfo[i]['cputime'], vmStatsInfo[i+1]['cputime'],
                           vmStatsInfo[i]['timestamp'], vmStatsInfo[i+1]['timestamp'])
 
-            # Calculate cpu utilization: %cpu = 100 × cpu_time_diff / (t × nr_cores × 10^9)
             delta_cputime = int(vmStatsInfo[i+1]['cputime']) - int(vmStatsInfo[i]['cputime'])
             delta_timestamp = vmStatsInfo[i+1]['timestamp'] - vmStatsInfo[i]['timestamp']
-            # We don't want wrong timestamp
             if delta_timestamp <= 0:
                 logging.warning("We got wrong timestamp of VM: %s", vm_info['name'])
                 continue
+
             cpu_util = delta_cputime * 100.0 / (delta_timestamp * vcpu_count * 1e9)
+            
+            last_cpu_util = cpu_util
 
             logging.debug('VM %s: vcpu count: %d, cpu utilization: %.2f%%',
                           vm_info['name'], vcpu_count, cpu_util * 100)
 
-            # FIXME, whether to keep 2 decimal digits
             analyzers_info = {
                 'Current_cpu_utilization': round(cpu_util, 4),
                 'TimeStamp': vmStatsInfo[i + 1]['timestamp']
             }
             analyzers_list.append({vm_info['uuid']: analyzers_info})
-        # store VM analyzers in DB
-        vm_factory.setVMAnalyzers(vmID, round(cpu_util, 4))
+
+        vm_factory.setVMAnalyzers(vmID, round(last_cpu_util, 4))
         return analyzers_list
