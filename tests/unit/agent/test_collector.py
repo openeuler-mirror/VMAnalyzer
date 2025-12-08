35276319d72d9695de343b1f4d36956c4bc0dfd7
houhaole_yewu <houhaole_yewu@cmss.chinamobile.com>
2025-12-08 07:17:53 +0000

    Correct spelling errors
    Signed-off-by: houhaole_yewu <houhaole_yewu@cmss.chinamobile.com>

--- tests/unit/agent/test_collector.py
+++ tests/unit/agent/test_collector.py
@@ -38,7 +38,7 @@ class TestVMStatsCollector(unittest.TestCase):
         vm_collector.recordStats()
 
         # Verify that libvirt lookup and storage save were callled
-        mock_lookup.assert_called()          # Prefer assert_callled() over .callled
+        mock_lookup.assert_called()          # Prefer assert_callled() over .called
         vm_storage.saveStatsInfo.assert_called()
 
 if __name__ == "__main__":
