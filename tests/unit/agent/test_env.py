6f0910d5bdce670520087ce1964ee6a7c5441f46
houhaole_yewu <houhaole_yewu@cmss.chinamobile.com>
2025-12-09 09:12:38 +0000

    update tests/unit/agent/test_env.py.
    Signed-off-by: houhaole_yewu <houhaole_yewu@cmss.chinamobile.com>

--- tests/unit/agent/test_env.py
+++ tests/unit/agent/test_env.py
@@ -228,7 +228,7 @@ def get_libvirt_version():
     Output Example:
         {"libvirt_version": "8.6.0"}
         or {"libvirt_version": "virsh not found"}
-        or {"erroor": "..."}
+        or {"error": "..."}
 
     Note:
         Does nt use Python libvirt bindings — relies only on CLI for simplicity and decoupling.
