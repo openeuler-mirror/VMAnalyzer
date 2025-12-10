95984339d1c533b45fa103b3fff677c07247dafd
baixu <baixu_yewu@cmss.chinamobile.com>
2025-12-10 09:28:46 +0000

    fix called spell
    Signed-off-by: baixu <baixu_yewu@cmss.chinamobile.com>

--- tests/unit/agent/test_collector.py
+++ tests/unit/agent/test_collector.py
@@ -38,7 +38,7 @@ class TestVMStatsCollector(unittest.TestCase):
         vm_collector.recordStats()
 
         # Verify that libvirt lookup and storage save were called
-        mock_lookup.assert_called()          # Prefer assert_callled() over .callled
+        mock_lookup.assert_called()          # Prefer assert_called() over .called
         # Verify that libvirt lookup and storage save were callled
         mock_lookup.assert_called()          # Prefer assert_callled() over .called
         mock_lookup.assert_called()          # Prefer assert_called() over .callled
