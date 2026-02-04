a5ae675c039f87aaddc450b0d7b8d77195ea432d
Xu Zheng <xuzheng_yewu@cmss.chinamobile.com>
2026-02-05 00:28:48 +0800

    Add main execution entry
    Signed-off-by: Xu Zheng <xuzheng_yewu@cmss.chinamobile.com>

--- diagnose/check_secret/check_secret.sh
+++ diagnose/check_secret/check_secret.sh
@@ -8,20 +8,31 @@ virt_dir=/var/log/vmanalyzer/
 
 mk_log_dir() {
     if [ ! -d "$virt_dir" ]; then
-mkdir -p $virt_dir
-fi
+        mkdir -p $virt_dir
+    fi
 }
 
 info() {
-echo "{\"status\": \"info\", \"log\": \"$1\"}" >> $check_log
+    echo "{\"status\": \"info\", \"log\": \"$1\"}" >> $check_log
 }
 
 error() {
-echo "{\"status\": \"error\", \"log\": \"$1\"}" >> $check_log
+    echo "{\"status\": \"error\", \"log\": \"$1\"}" >> $check_log
 }
 
 warn() {
-echo "{\"status\": \"warning\", \"log\": \"$1\"}" >> $check_log
+    echo "{\"status\": \"warning\", \"log\": \"$1\"}" >> $check_log
 }
 
 check_log=${virt_dir}check_secret-$(date "+%Y-%m-%d-%H-%M-%S").log
+
+# 主函数
+main() {
+    mk_log_dir
+    echo "####################################################################################" > $check_log
+    time=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "$time" >> $check_log
+}
+
+# 执行主函数
+main
