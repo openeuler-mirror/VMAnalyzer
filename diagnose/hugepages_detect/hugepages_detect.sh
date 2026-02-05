3f00971f93d21294b452590244d5bdf52cf0417c
Xu Zheng <xuzheng_yewu@cmss.chinamobile.com>
2026-02-05 17:52:03 +0800

    Add log warn and error output helpers
    Signed-off-by: Xu Zheng <xuzheng_yewu@cmss.chinamobile.com>

--- diagnose/hugepages_detect/hugepages_detect.sh
+++ diagnose/hugepages_detect/hugepages_detect.sh
@@ -10,3 +10,21 @@ YELLOW_BLINK="\e[1;33m"
 RED="\e[1;31m"
 NICLISTS=""
 RLNICS=""
+
+function warn_info()
+{
+    echo -e "${YELLOW_BLINK} Notice:${NORMAL} ${RED}$1 ${NORMAL}" >> $LOG_PATH
+}
+
+function err_info()
+{
+    time=`date +"%Y-%m-%d %H:%M:%S"`
+    echo -e "$time ${YELLOW_BLINK} Error:${NORMAL} ${RED}$1 ${NORMAL}" >> $LOG_PATH
+}
+
+function log()
+{
+    time=`date +"%Y-%m-%d %H:%M:%S"`
+    echo -e "\e[1m $time $1 \e[0;39m" >> $LOG_PATH
+    echo ""
+}
