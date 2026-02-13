3267127f62044c79461103ae29380f99d62137f4
qihao_yewu <qihao_yewu@cmss.chinamobile.com>
2026-02-13 10:20:39 +0800

    modify the name to make it more readable for memory status

--- gather/get_vm_memory_status.py
+++ gather/get_vm_memory_status.py
@@ -93,13 +93,13 @@ class VMCollector:
     def collect_single_vm_data(self, vm_name: str) -> Dict:
         LOG_INFO(f"\n===== 开始采集虚拟机：{vm_name} =====")
         
-        tcp_conn_state = self.call_qga_interface(vm_name, "guest-get-memory-status")
+        memory_status = self.call_qga_interface(vm_name, "guest-get-memory-status")
         vm_data = {
             "name": vm_name,
             "get_memory_ststus": {
                 "interface": "guest-get-memory-status",
-                "error": tcp_conn_state["error"],
-                "data": tcp_conn_state["data"]
+                "error": memory_status["error"],
+                "data": memory_status["data"]
             },
         }
         LOG_INFO(f"===== 虚拟机 {vm_name} 采集完成 =====")
