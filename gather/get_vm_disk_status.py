844d7ce68255ef24c4aad4669798d4853e4c924c
qihao_yewu <qihao_yewu@cmss.chinamobile.com>
2026-02-13 09:18:12 +0800

    Change the name to make it more readable for disk status

--- gather/get_vm_disk_status.py
+++ gather/get_vm_disk_status.py
@@ -93,13 +93,13 @@ class VMCollector:
     def collect_single_vm_data(self, vm_name: str) -> Dict:
         LOG_INFO(f"\n===== 开始采集虚拟机：{vm_name} =====")
         
-        tcp_conn_state = self.call_qga_interface(vm_name, "guest-get-disk-status")
+        disk_status = self.call_qga_interface(vm_name, "guest-get-disk-status")
         vm_data = {
             "name": vm_name,
             "get_disk_status": {
                 "interface": "guest-get-disk-status",
-                "error": tcp_conn_state["error"],
-                "data": tcp_conn_state["data"]
+                "error": disk_status["error"],
+                "data": disk_status["data"]
             },
         }
         LOG_INFO(f"===== 虚拟机 {vm_name} 采集完成 =====")
