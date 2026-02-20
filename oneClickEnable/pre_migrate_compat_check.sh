11921f86f42eef484e6411b9091d7d34cb6ad8c6
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-02-20 23:42:03 +0800

    check command

---
+++ oneClickEnable/pre_migrate_compat_check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+log_info() {
+    echo -e "${GREEN}[INFO]${NC} $1"
+}
+
+log_warn() {
+    echo -e "${YELLOW}[WARN]${NC} $1"
+}
+
+log_error() {
+    echo -e "${RED}[ERROR]${NC} $1"
+}
+
+check_command() {
+    if ! command -v $1 &> /dev/null; then
+        log_error "命令 $1 未找到，请安装相关软件包"
+        exit 1
+    fi
+}
+
