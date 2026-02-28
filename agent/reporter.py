f3281b2039821b0d90e32c27c850b1a3e9e6babe
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-02-28 17:15:08 +0800

    Code specification modification

--- agent/reporter.py
+++ agent/reporter.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # _*_coding: utf-8 _*_
-#######################################################################################
+
 # Copyright (c) 2023. China Mobile (SuZhou) Software Technology Co.,Ltd.
 # VMAnalyzer is licensed under Mulan PSL v2.
-# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You can use this software according to the terms and conditions of
+# the Mulan PSL v2.
 # You may obtain a copy of Mulan PSL v2 at:
 #          http://license.coscl.org.cn/MulanPSL2
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 # EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
-#######################################################################################
 import time
 from utils import config
+
 
 class VMAnalyzersReporter():
     """
@@ -42,6 +43,9 @@ class VMAnalyzersReporter():
         end_time = time.time()
         start_time = end_time - config.VM_ANALYZERS_CONFIG['duration'] * interval
         for vm_id in list(vm_factory.vms.keys()):
-            vm_stats = self.__stats_storage.get_stats_info(vm_id, start_time, end_time)
+            vm_stats = self.__stats_storage.get_stats_info(vm_id,
+                                                          start_time, end_time)
             vm_analyzers = self.__stats_analyzer.analyze_stats(vm_id, vm_stats)
             self.__analyzers_viewer.output(vm_analyzers)
+
+
