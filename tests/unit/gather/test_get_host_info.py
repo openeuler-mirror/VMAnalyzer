a56938f1ab8428b869f6e3d2d871cd4eeb00ffe3
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-02 23:22:02 +0800

    Import standard library dependencies

--- tests/unit/gather/test_get_host_info.py
+++ tests/unit/gather/test_get_host_info.py
@@ -11,3 +11,7 @@
 # EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
+
+import unittest
+from unittest import mock
+from gather.get_host_info import HostHypervisorCollector
