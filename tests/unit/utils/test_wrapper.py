7d4829210187e5794aa91de5e810189ded783ae7
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-02 23:11:23 +0800

    Import standard library dependencies

--- tests/unit/utils/test_wrapper.py
+++ tests/unit/utils/test_wrapper.py
@@ -11,3 +11,10 @@
 # EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
+import unittest
+import logging
+import sys
+import os
+from io import StringIO
+
+from utils.wrapper import singleton
