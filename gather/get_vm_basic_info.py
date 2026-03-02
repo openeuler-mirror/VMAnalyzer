47f6d813254ab05f2ec2d43a63db070db79541bb
qihao_yewu <qihao_yewu@cmss.chinamobile.com>
2026-03-02 16:50:21 +0800

    Increase timeout by 30 seconds to avoid command freezing

--- gather/get_vm_basic_info.py
+++ gather/get_vm_basic_info.py
@@ -30,7 +30,8 @@ class VMDomainMonitor:
                 cmd.split(),
                 capture_output=True,
                 text=True,
-                check=True
+                check=True,
+                timeout=30
             )
             return result.stdout.strip()
         except subprocess.CalledProcessError as e:
