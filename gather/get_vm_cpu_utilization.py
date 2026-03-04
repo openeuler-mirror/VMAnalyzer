a9dc9d43958ee469c86286c67fa580038c48e0e2
qihao_yewu <qihao_yewu@cmss.chinamobile.com>
2026-03-04 19:29:55 +0800

    Replace the obsolete parameter 'universal_newlines' ->' text '

--- gather/get_vm_cpu_utilization.py
+++ gather/get_vm_cpu_utilization.py
@@ -52,7 +52,7 @@ class VMCollector:
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 shell=True,
-                universal_newlines=True,
+                text=True,
                 check=True,
                 timeout=self.cmd_timeout  # 超时保护，避免命令卡死
             )
