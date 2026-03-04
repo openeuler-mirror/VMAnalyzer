786e401471cd8e06167b6024f040249d8464e3c6
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-04 22:50:11 +0800

    Assert running vm list parsing and command call

--- tests/unit/gather/test_get_vm_cpu_top_app.py
+++ tests/unit/gather/test_get_vm_cpu_top_app.py
@@ -29,6 +29,10 @@ class TestVMCPUTopNCollector(unittest.TestCase):
     def test_get_running_vm_names(self, mock_run):
         mock_run.return_value = "vm1\nvm2\nvm3"
         vms = self.collector.get_running_vm_names()
+        self.assertEqual(vms, ["vm1", "vm2", "vm3"])
+        mock_run.assert_called_once_with(
+            "virsh list --name | grep -v '^$'"
+        )
 
 if __name__ == "__main__":
     unittest.main()
