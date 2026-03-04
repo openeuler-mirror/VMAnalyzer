b39ba9f4bfe70ff85a9910f3f1315150674c799d
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-04 22:55:47 +0800

    Add tearDown method for VMAnalyzer tests

--- tests/unit/gather/test_get_vm_fs_info.py
+++ tests/unit/gather/test_get_vm_fs_info.py
@@ -31,5 +31,8 @@ class TestVMAnalyzer(unittest.TestCase):
     def setUp(self):
         self.analyzer = vm_fs_info.VMAnalyzer()
 
+    def tearDown(self):
+        self.analyzer = None
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
