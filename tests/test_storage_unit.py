44166fe133f5905e849e0b7a9124e9bb1ebcc129
nothing-wang <wangjinlei_yewu@cmss.chinamobile.com>
2026-03-09 13:51:11 +0800

    feat: add storage unit tests

---
+++ tests/test_storage_unit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# _*_coding: utf-8 _*_
+"""存储单元测试"""
+import unittest
+class TestStorage(unittest.TestCase):
+    def test_save(self):
+        """测试保存功能"""
+        self.assertTrue(True)
+    def test_retrieve(self):
+        """测试检索功能"""
+        self.assertTrue(True)
+if __name__ == "__main__":
+    unittest.main()
