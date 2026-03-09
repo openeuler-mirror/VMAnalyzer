beeb15b4820db49c7bace7ada4e6a813cda7267e
nothing-wang <wangjinlei_yewu@cmss.chinamobile.com>
2026-03-09 13:51:11 +0800

    feat: add simple cache manager

---
+++ utils/cache_manager.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# _*_coding: utf-8 _*_
+"""简单缓存管理器"""
+import time
+class CacheManager:
+    def __init__(self, ttl=300):
+        self.cache = {}
+        self.ttl = ttl
+    def set(self, key, value):
+        self.cache[key] = (value, time.time())
+    def get(self, key):
+        if key in self.cache:
+            value, timestamp = self.cache[key]
+            if time.time() - timestamp < self.ttl:
+                return value
+            del self.cache[key]
+        return None
