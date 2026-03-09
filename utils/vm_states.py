c4b3846d172ad5798530fcf3957783d4fb67d8c2
nothing-wang <wangjinlei_yewu@cmss.chinamobile.com>
2026-03-09 13:51:11 +0800

    feat: add VM state constants

---
+++ utils/vm_states.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# _*_coding: utf-8 _*_
+"""VM状态常量定义"""
+VM_STATE_RUNNING = "running"
+VM_STATE_PAUSED = "paused"
+VM_STATE_SHUTDOWN = "shut off"
+VM_STATE_CRASHED = "crashed"
+VM_STATE_PMSUSPENDED = "pmsuspended"
+VM_STATES = [VM_STATE_RUNNING, VM_STATE_PAUSED, VM_STATE_SHUTDOWN, VM_STATE_CRASHED, VM_STATE_PMSUSPENDED]
