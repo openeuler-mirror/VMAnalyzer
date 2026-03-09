62bb8c2f38e8775ddbc3f4a32fae1b8755dd846b
nothing-wang <wangjinlei_yewu@cmss.chinamobile.com>
2026-03-09 13:51:11 +0800

    feat: add alert level constants

---
+++ utils/alert_levels.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_coding: utf-8 _*_
+"""告警级别常量"""
+ALERT_LEVEL_INFO = "info"
+ALERT_LEVEL_WARNING = "warning"
+ALERT_LEVEL_ERROR = "error"
+ALERT_LEVEL_CRITICAL = "critical"
+ALERT_LEVELS = [ALERT_LEVEL_INFO, ALERT_LEVEL_WARNING, ALERT_LEVEL_ERROR, ALERT_LEVEL_CRITICAL]
