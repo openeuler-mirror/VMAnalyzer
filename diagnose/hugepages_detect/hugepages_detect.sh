991945badfbb6e34049c9902afe1acef636b8a9c
dinglimin <dinglimin@cmss.chinamobile.com>
2026-03-10 11:06:37 +0800

    Check if the PID is valid
    Signed-off-by: dinglimin <dinglimin@cmss.chinamobile.com>

--- diagnose/hugepages_detect/hugepages_detect.sh
+++ diagnose/hugepages_detect/hugepages_detect.sh
@@ -65,6 +65,11 @@ function record_vm_hugepages()
     for vm in ${running_vms[@]}
     do
         pid=`ps -ef | grep qemu | grep $vm | awk '{print $2}'`
+        # 检查pid是否有效
+        if [ -z "$pid" ] || [ ! -f "/proc/$pid/smaps" ]; then
+            warn_info "VM $vm pid $pid not found or smaps file not exist"
+            continue
+        fi
         total_2M=`grep -B 11 'KernelPageSize: 2048 kB' /proc/$pid/smaps | grep "^Size:" | awk 'BEGIN{sum=0}{sum+=$2}END{print sum/1024}'`
         total_1G=`grep -B 11 'KernelPageSize: 1048576 kB' /proc/$pid/smaps | grep "^Size:" | awk 'BEGIN{sum=0}{sum+=$2}END{print sum/1048576}'`
         log "$vm occupied $total_2M M, occupied $total_1G G /n"
