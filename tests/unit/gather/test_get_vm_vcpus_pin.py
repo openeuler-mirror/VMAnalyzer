c3359d422a92bbbafc4ea227b6050716f3bcf4dc
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-10 23:04:01 +0800

    Import standard library dependencies

--- tests/unit/gather/test_get_vm_vcpus_pin.py
+++ tests/unit/gather/test_get_vm_vcpus_pin.py
@@ -11,3 +11,7 @@
 # EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
+import unittest
+from unittest.mock import patch, MagicMock
+import subprocess
+import gather.get_vm_vcpus_pin as get_vm_vcpus_pin
