9bf5a3ea376fe9c851e05cfdb19edd14bb37da2a
nothing-wang <wangjinlei_yewu@cmss.chinamobile.com>
2026-03-12 10:27:35 +0800

    fix: add missing __init__.py to diagnose module

---
+++ diagnose/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+VMAnalyzer diagnose module.
+
+This module provides diagnostic tools for VM performance and issues.
+"""
+
+__version__ = "0.1.0"
