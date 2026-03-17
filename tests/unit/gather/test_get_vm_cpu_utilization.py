fbbeac96c44ba4e96641f917abad9e7fc6db23b2
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-17 17:19:06 +0800

    Verify get_all_vm_names returns correct VM list

--- tests/unit/gather/test_get_vm_cpu_utilization.py
+++ tests/unit/gather/test_get_vm_cpu_utilization.py
@@ -51,3 +51,5 @@ class TestVMInfo(unittest.TestCase):
     @patch.object(get_vm_cpu_utilization.VMCollector, "run_virsh_cmd")
     def test_get_all_vm_names(self, mock_cmd):
         mock_cmd.return_value = "vm1\nvm2\n"
+        result = self.collector.get_all_vm_names()
+        self.assertEqual(result, ["vm1", "vm2"])
