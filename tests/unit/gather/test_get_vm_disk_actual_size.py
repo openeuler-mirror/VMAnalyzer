a48212797845717afc7bbfd64fb43475fb617f6f
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-19 21:57:48 +0800

    Add mocks for test_execute_cmd_timeout

--- tests/unit/gather/test_get_vm_disk_actual_size.py
+++ tests/unit/gather/test_get_vm_disk_actual_size.py
@@ -31,5 +31,10 @@ class TestVMDiskActualSize(unittest.TestCase):
         self.assertEqual(result["code"], 0)
         self.assertEqual(result["stdout"], "ok")
 
+    @patch("gather.get_vm_disk_actual_size.subprocess.run")
+    def test_execute_cmd_timeout(self, mock_run):
+        from subprocess import TimeoutExpired
+        mock_run.side_effect = TimeoutExpired(cmd="cmd", timeout=30)
+
 if __name__ == "__main__":
     unittest.main()
