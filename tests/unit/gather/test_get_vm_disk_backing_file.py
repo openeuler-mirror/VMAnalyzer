1692d83fff40a33fa7e63df18f799cf8b2ec1aae
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-19 21:59:55 +0800

    Add mocks for test_execute_cmd_timeout

--- tests/unit/gather/test_get_vm_disk_backing_file.py
+++ tests/unit/gather/test_get_vm_disk_backing_file.py
@@ -31,5 +31,10 @@ class TestVMDiskBackingFile(unittest.TestCase):
         self.assertEqual(result["code"], 0)
         self.assertEqual(result["stdout"], "ok")
 
+    @patch("gather.get_vm_disk_backing_file.subprocess.run")
+    def test_execute_cmd_timeout(self, mock_run):
+        from subprocess import TimeoutExpired
+        mock_run.side_effect = TimeoutExpired(cmd="cmd", timeout=30)
+
 if __name__ == "__main__":
     unittest.main()
