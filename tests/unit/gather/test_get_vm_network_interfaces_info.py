a07ffddf71d514288a1e9689bb3a93a44350491a
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-20 18:18:18 +0800

    Add mocks for test_get_all_vm_names

--- tests/unit/gather/test_get_vm_network_interfaces_info.py
+++ tests/unit/gather/test_get_vm_network_interfaces_info.py
@@ -50,5 +50,12 @@ class TestVMCollector(unittest.TestCase):
         result = self.collector.run_virsh_cmd("cmd")
         self.assertIsNone(result)
 
+    # =========================
+    # get_all_vm_names
+    # =========================
+    @patch.object(get_vm_network_interfaces_info.VMCollector, "run_virsh_cmd")
+    def test_get_all_vm_names(self, mock_cmd):
+        mock_cmd.return_value = "vm1\nvm2\n"
+
 if __name__ == "__main__":
     unittest.main()
