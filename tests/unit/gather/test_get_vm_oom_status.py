1c0ba2c53e8d8e32bef9ffe95b6735c45a01ab44
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-24 00:38:59 +0800

    Add setUp initialization for TestVMOOMCollector

--- tests/unit/gather/test_get_vm_oom_status.py
+++ tests/unit/gather/test_get_vm_oom_status.py
@@ -18,7 +18,11 @@ import json
 from gather import get_vm_oom_status
 
 class TestVMOOMCollector(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.collector = get_vm_oom_status.VMCollector(
+            output_dir="./test_output"
+        )
 
 if __name__ == "__main__":
     unittest.main()
