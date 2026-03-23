1738009f4ae6257374284d457c528ff1297e9b4f
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-24 00:41:37 +0800

    Add mocks for test_execute_cmd_success

--- tests/unit/gather/test_get_vm_snapshot_info.py
+++ tests/unit/gather/test_get_vm_snapshot_info.py
@@ -18,7 +18,15 @@ import json
 from gather import get_vm_snapshot_info
 
 class TestVMSnapshotInfo(unittest.TestCase):
-    pass
+
+    # =========================
+    # execute_cmd
+    # =========================
+    @patch("gather.get_vm_snapshot_info.subprocess.run")
+    def test_execute_cmd_success(self, mock_run):
+        mock_run.return_value.returncode = 0
+        mock_run.return_value.stdout = "ok\n"
+        mock_run.return_value.stderr = ""
 
 if __name__ == "__main__":
     unittest.main()
