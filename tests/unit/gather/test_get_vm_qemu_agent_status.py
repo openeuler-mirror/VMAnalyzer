1e558b7a1a06b626aeaf48845821454d1d67ecec
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-25 16:58:35 +0800

    Add TestVMQemuAgentStatus test skeleton

--- tests/unit/gather/test_get_vm_qemu_agent_status.py
+++ tests/unit/gather/test_get_vm_qemu_agent_status.py
@@ -16,3 +16,9 @@ from unittest.mock import patch
 import json
 
 from gather import get_vm_qemu_agent_status
+
+class TestVMQemuAgentStatus(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
