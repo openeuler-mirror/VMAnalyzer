46e4825cfac4aeab32048ddba0403a86ce61ae25
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-25 14:04:11 +0800

    Import standard library dependencies

--- tests/unit/gather/test_get_vm_qemu_agent_status.py
+++ tests/unit/gather/test_get_vm_qemu_agent_status.py
@@ -11,3 +11,8 @@
 # EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
+import unittest
+from unittest.mock import patch
+import json
+
+from gather import get_vm_qemu_agent_status
