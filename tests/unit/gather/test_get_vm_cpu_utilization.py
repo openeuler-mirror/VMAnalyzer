e1d610ff933d70c0a5840a0f0261cb431988c5da
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-25 10:20:55 +0800

    Add TestSaveData class and patch setup for save_data

--- tests/unit/gather/test_get_vm_cpu_utilization.py
+++ tests/unit/gather/test_get_vm_cpu_utilization.py
@@ -140,3 +140,9 @@ class TestCollectAll(unittest.TestCase):
             len(collector.all_vms_data["vms"]),
             2
         )
+
+class TestSaveData(unittest.TestCase):
+    @patch("builtins.open", new_callable=mock_open)
+    @patch("gather.get_vm_cpu_utilization.datetime")
+    def test_save_data(self, mock_datetime, mock_file):
+        mock_datetime.now.return_value.strftime.return_value = "20240101_120000"
