005d00d070e8d0a4272476840c5a06e095af0743
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-25 10:24:10 +0800

    Add assertions for save_data

--- tests/unit/gather/test_get_vm_cpustinfo.py
+++ tests/unit/gather/test_get_vm_cpustinfo.py
@@ -108,3 +108,4 @@ class TestVMCpuStInfoCollector(unittest.TestCase):
     @patch("gather.get_vm_cpustinfo.json.dump")
     def test_save_data(self, mock_json_dump, mock_file):
         self.collector.save_data()
+        mock_json_dump.assert_called()
