1dca3608aade726186f7c0b9bf7476f640b469c3
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-26 15:42:35 +0800

    Add assertions for test_close_connection_success

--- tests/unit/gather/test_getlibver.py
+++ tests/unit/gather/test_getlibver.py
@@ -41,6 +41,8 @@ class TestGetLibVer(unittest.TestCase):
     # =========================
     def test_close_connection_success(self):
         mock_conn = MagicMock()
+        getlibver.closeConnection(mock_conn)
+        mock_conn.close.assert_called_once()
 
 if __name__ == "__main__":
     unittest.main()
