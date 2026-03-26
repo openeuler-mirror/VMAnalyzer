ad2cbbab61089e84b06647132b6a3cd7ac29ab45
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-03-26 10:18:50 +0800

    Add mocks for test_execute_cmd_timeout

--- tests/unit/gather/test_get_vm_qemu_agent_status.py
+++ tests/unit/gather/test_get_vm_qemu_agent_status.py
@@ -31,5 +31,13 @@ class TestVMQemuAgentStatus(unittest.TestCase):
         self.assertEqual(result["code"], 0)
         self.assertEqual(result["stdout"], "ok")
 
+    @patch("gather.get_vm_qemu_agent_status.subprocess.run")
+    def test_execute_cmd_timeout(self, mock_run):
+        from subprocess import TimeoutExpired
+        mock_run.side_effect = TimeoutExpired(
+            cmd="cmd",
+            timeout=30
+        )
+
 if __name__ == "__main__":
     unittest.main()
