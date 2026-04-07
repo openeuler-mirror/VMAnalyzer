fb3d3d3ef5bf7c3b6544dd21fb4b16c9d26bd2d9
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-04-07 14:44:22 +0800

    Add mocks for test_run_virsh_cmd_success

--- tests/unit/gather/test_get_vm_cpu_flags.py
+++ tests/unit/gather/test_get_vm_cpu_flags.py
@@ -18,7 +18,16 @@ import subprocess
 from gather import get_vm_cpu_cache_topology
 
 class TestGetVmCpuFlags(unittest.TestCase):
-    pass
+    # =========================
+    # run_virsh_cmd
+    # =========================
+    @patch("gather.get_vm_cpu_flags.subprocess.run")
+    def test_run_virsh_cmd_success(self, mock_run):
+        mock_run.return_value = MagicMock(
+            returncode=0,
+            stdout="ok",
+            stderr=""
+        )
 
 
 if __name__ == "__main__":
