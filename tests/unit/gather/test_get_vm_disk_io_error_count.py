88e72e6b13f840e8bba53eda15120fbe3bc00dd0
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-04-09 14:27:47 +0800

    Add assertions for test_get_vm_disk_list_success

--- tests/unit/gather/test_get_vm_disk_io_error_count.py
+++ tests/unit/gather/test_get_vm_disk_io_error_count.py
@@ -80,6 +80,15 @@ class TestGetVmDiskIoErrorCount(unittest.TestCase):
             "stdout": stdout,
             "stderr": ""
         }
+        result = (
+            get_vm_disk_io_error_count
+            .get_vm_disk_list("vm1")
+        )
+        self.assertEqual(len(result), 2)
+        self.assertEqual(
+            result[0]["target"],
+            "vda"
+        )
 
 if __name__ == "__main__":
     unittest.main()
