4e825975c7b7d04d23430eac94106af3abf9c423
ynynyn7 <yanning_yewu@cmss.chinamobile.com>
2026-04-10 10:18:49 +0800

    Add mocks for test_run_virsh_cmd_exception

--- tests/unit/gather/test_get_vm_cpu_flags.py
+++ tests/unit/gather/test_get_vm_cpu_flags.py
@@ -50,5 +50,9 @@ class TestGetVmCpuFlags(unittest.TestCase):
         result = get_vm_cpu_flags.run_virsh_cmd("cmd")
         self.assertIsNone(result)
 
+    @patch("gather.get_vm_cpu_flags.subprocess.run")
+    def test_run_virsh_cmd_exception(self, mock_run):
+        mock_run.side_effect = Exception("boom")
+
 if __name__ == "__main__":
     unittest.main()
