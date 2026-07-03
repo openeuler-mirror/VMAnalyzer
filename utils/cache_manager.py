728f6ce3ae646b07ddf5fd99b78578dd3aa26e6a
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-03 20:32:33 +0800

    add has_key method

--- utils/cache_manager.py
+++ utils/cache_manager.py
@@ -8,6 +8,8 @@ class CacheManager:
         self.ttl = ttl
     def set(self, key, value):
         self.cache[key] = (value, time.time())
+    def has_key(self, key):
+        return key in self.cache
     def get(self, key):
         if key in self.cache:
             value, timestamp = self.cache[key]
