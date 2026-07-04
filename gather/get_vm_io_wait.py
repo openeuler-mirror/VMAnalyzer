366ffc04333460709844ed30805325b7a39fe215
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-04 15:58:09 +0800

    style: translate messages in gather/get_vm_io_wait.py

--- gather/get_vm_io_wait.py
+++ gather/get_vm_io_wait.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # _*_coding: utf-8 _*_
-"""采集虚机IO等待统计"""
+"""Documentation for this component."""
 import json
 def main():
     print(json.dumps({"feature": "VM IO wait collector", "status": "ok"}, ensure_ascii=False))
