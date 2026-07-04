af170c075cbce82e6d06f813b240443dd0e6d27e
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-04 15:58:37 +0800

    style: translate messages in gather/get_vm_swap_usage.py

--- gather/get_vm_swap_usage.py
+++ gather/get_vm_swap_usage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # _*_coding: utf-8 _*_
-"""采集虚机Swap使用情况"""
+"""Documentation for this component."""
 import json
 def main():
     print(json.dumps({"feature": "VM swap usage collector", "status": "ok"}, ensure_ascii=False))
