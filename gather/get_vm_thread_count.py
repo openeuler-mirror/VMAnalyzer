876c1ae3e85b10a5ec606b077cc01977c2d93fdf
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-04 15:58:40 +0800

    style: translate messages in gather/get_vm_thread_count.py

--- gather/get_vm_thread_count.py
+++ gather/get_vm_thread_count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # _*_coding: utf-8 _*_
-"""采集虚机线程数量"""
+"""Documentation for this component."""
 import json
 def main():
     print(json.dumps({"feature": "VM thread count collector", "status": "ok"}, ensure_ascii=False))
