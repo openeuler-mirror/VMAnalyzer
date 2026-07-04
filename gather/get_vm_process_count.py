8dd0086e5a9f08b03f1e133d099dee466babe65d
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-04 15:58:29 +0800

    style: translate messages in gather/get_vm_process_count.py

--- gather/get_vm_process_count.py
+++ gather/get_vm_process_count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # _*_coding: utf-8 _*_
-"""采集虚机进程数量"""
+"""Documentation for this component."""
 import json
 def main():
     print(json.dumps({"feature": "VM process count collector", "status": "ok"}, ensure_ascii=False))
