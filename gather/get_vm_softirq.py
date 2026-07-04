ca0378c72d96c64d41dc1c9534af40ca8793d055
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-04 15:58:34 +0800

    style: translate messages in gather/get_vm_softirq.py

--- gather/get_vm_softirq.py
+++ gather/get_vm_softirq.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # _*_coding: utf-8 _*_
-"""采集虚机软中断统计"""
+"""Documentation for this component."""
 import json
 def main():
     print(json.dumps({"feature": "VM softirq collector", "status": "ok"}, ensure_ascii=False))
