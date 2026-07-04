6839011b09321090931b3c207ed1b143faa787c2
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-04 15:59:02 +0800

    style: translate messages in utils/unit_converter.py

--- utils/unit_converter.py
+++ utils/unit_converter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # _*_coding: utf-8 _*_
-"""单位转换工具"""
+"""Documentation for this component."""
 def bytes_to_mb(bytes_val):
     return bytes_val / (1024 * 1024)
 def bytes_to_gb(bytes_val):
