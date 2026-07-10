bb27b11f0d6b6743a9d6d7095c4126555e52435e
lamerpoet <xuzheng_yewu@cmss.chinamobile.com>
2026-07-10 09:29:51 +0800

    feat: add lru_cache - LRU cache with OrderedDict

---
+++ utils/lru_cache.py
@@ -0,0 +1,37 @@
+﻿#!/usr/bin/env python3
+"""LRU cache implementation using OrderedDict."""
+from collections import OrderedDict
+from typing import Any, Optional
+
+class LRUCache:
+    def __init__(self, capacity: int = 128):
+        self._capacity = capacity
+        self._cache = OrderedDict()
+
+    def get(self, key: Any) -> Optional[Any]:
+        if key not in self._cache:
+            return None
+        self._cache.move_to_end(key)
+        return self._cache[key]
+
+    def put(self, key: Any, value: Any) -> None:
+        if key in self._cache:
+            self._cache.move_to_end(key)
+        elif len(self._cache) >= self._capacity:
+            self._cache.popitem(last=False)
+        self._cache[key] = value
+
+    def remove(self, key: Any) -> bool:
+        if key in self._cache:
+            del self._cache[key]
+            return True
+        return False
+
+    def clear(self) -> None:
+        self._cache.clear()
+
+    def size(self) -> int:
+        return len(self._cache)
+
+    def keys(self) -> list:
+        return list(self._cache.keys())
